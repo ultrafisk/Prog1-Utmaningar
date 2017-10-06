2aafca2b6875d3f3a481439eed602f7d7c2a86fa
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-10-06 10:27:47 +0200

    Added Multiplikationstabell
    Added the utmaning Multiplikationstabell

---
+++ Multiplikationstabell/Multiplikationstabell/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplikationstabell
+{
+    class Program   //Erik Hacker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplikationstabellinator-5 Billion");
+            Console.WriteLine("Skriv in ett tal och och sedan skrivs talets multiplikationstabell ut.");
+            string userInput = Console.ReadLine();
+            int parsedInput = int.Parse(userInput);
+            int[] multiplikationer = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                multiplikationer[i] = (i+1) * parsedInput;
+            }
+            foreach(int nummer in multiplikationer)
+            {
+                Console.WriteLine(nummer);
+            }
+            Console.ReadLine();
+        }
+    }
+}
