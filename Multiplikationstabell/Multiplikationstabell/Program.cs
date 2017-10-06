087194de2d97762b62e3158ad63c9e494f369487
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-10-06 10:35:25 +0200

    Added Multiplikationstabell

--- Multiplikationstabell/Multiplikationstabell/Program.cs
+++ Multiplikationstabell/Multiplikationstabell/Program.cs
@@ -11,15 +11,15 @@ namespace Multiplikationstabell
         static void Main(string[] args)
         {
             Console.WriteLine("Multiplikationstabellinator-5 Billion");
-            Console.WriteLine("Skriv in ett tal och och sedan skrivs talets multiplikationstabell ut.");
-            string userInput = Console.ReadLine();
-            int parsedInput = int.Parse(userInput);
-            int[] multiplikationer = new int[10];
+            Console.WriteLine("Skriv in ett tal och och sedan skrivs talets multiplikationstabell ut.");    //Skriver ut instruktioner
+            string userInput = Console.ReadLine();  //gör en sträng som är lika med vad användaren skriver in
+            int parsedInput = int.Parse(userInput); //omvandlar stängen till en integer
+            int[] multiplikationer = new int[10];   //tabell som multiplicerar input med talen 1 till 10
             for (int i = 0; i < 10; i++)
             {
                 multiplikationer[i] = (i+1) * parsedInput;
             }
-            foreach(int nummer in multiplikationer)
+            foreach(int nummer in multiplikationer) //skriver ut varje värde i tabellen som är talet multiplicerat med 1 - 10
             {
                 Console.WriteLine(nummer);
             }
