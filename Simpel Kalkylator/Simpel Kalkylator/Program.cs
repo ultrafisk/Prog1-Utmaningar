6fed80733a3ff5117a6dc35cec0ec64ee0481711
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-10-14 18:09:48 +0200

    Complete
    Completed Subtraktion and Multiplikatrion and added comments

--- Simpel Kalkylator/Simpel Kalkylator/Program.cs
+++ Simpel Kalkylator/Simpel Kalkylator/Program.cs
@@ -10,34 +10,40 @@ namespace Simpel_Kalkylator //Erik Hacker
     {
         static void Main(string[] args)
         {
+            //Skriver Hej
             Console.Write("Hej!");
 
+            //Kör Metoden MathChoice
             MathChoice();
 
             Console.ReadLine();
         }
         static void MathChoice()
         {
+            //Skriver Valen
             Console.Write("Välj ett räknesätt:\n1.Addition\n2.Subtraktion\n3.Multiplikation\n");
 
-            int tal;
-            bool inputValid = int.TryParse(Console.ReadLine(), out tal);
+            //Kollar input och parse-ar
+            int val;
+
+            bool inputValid = int.TryParse(Console.ReadLine(), out val);
 
             if (!inputValid)
             {
-                Console.WriteLine("Felaktigt input, försök igen.");
+                Console.WriteLine("Felaktigt tal, försök igen.");
                 MathChoice();
             }
 
-            if (tal == 1)
+            //Kollar valet och kör tillhörande metod eller skriver om det är ett felaktigt val
+            if (val == 1)
             {
                 Addition();
             }
-            else if (tal == 2)
+            else if (val == 2)
             {
                 Subtraktion();
             }
-            else if (tal == 3)
+            else if (val == 3)
             {
                 Multiplikation();
             }
@@ -46,12 +52,18 @@ namespace Simpel_Kalkylator //Erik Hacker
                 Console.WriteLine("Felaktigt val, försök igen:");
                 MathChoice();
             }
+
+            //Kör denna metod igen
+            MathChoice();
         }
+
+        //Tar input och adderar dem
         static void Addition()
         {
             Console.WriteLine("Skriv ditt första tal: ");
 
             int input1;
+
             bool input1Valid = int.TryParse(Console.ReadLine(), out input1);
 
             if (!input1Valid)
@@ -74,13 +86,65 @@ namespace Simpel_Kalkylator //Erik Hacker
             int output = input1 + input2;
             Console.WriteLine($"{input1} + {input2} = {output}");
         }
+
+        //tar input och subtraherar dem
         static void Subtraktion()
         {
-            
+            Console.WriteLine("Skriv ditt första tal: ");
+
+            int input1;
+
+            bool input1Valid = int.TryParse(Console.ReadLine(), out input1);
+
+            if (!input1Valid)
+            {
+                Console.WriteLine("Felaktigt tal, försök igen.");
+                Subtraktion();
+            }
+
+            Console.WriteLine("Skriv in ditt andra tal: ");
+
+            int input2;
+            bool input2Valid = int.TryParse(Console.ReadLine(), out input2);
+
+            if (!input2Valid)
+            {
+                Console.WriteLine("Felaktigt tal, försök igen.");
+                Subtraktion();
+            }
+
+            int output = input1 - input2;
+            Console.WriteLine($"{input1} - {input2} = {output}");
         }
+
+        //tar input och multiplicerar dem
         static void Multiplikation()
         {
+            Console.WriteLine("Skriv ditt första tal: ");
 
+            int input1;
+
+            bool input1Valid = int.TryParse(Console.ReadLine(), out input1);
+
+            if (!input1Valid)
+            {
+                Console.WriteLine("Felaktigt tal, försök igen.");
+                Multiplikation();
+            }
+
+            Console.WriteLine("Skriv in ditt andra tal: ");
+
+            int input2;
+            bool input2Valid = int.TryParse(Console.ReadLine(), out input2);
+
+            if (!input2Valid)
+            {
+                Console.WriteLine("Felaktigt tal, försök igen.");
+                Multiplikation();
+            }
+
+            int output = input1 * input2;
+            Console.WriteLine($"{input1} * {input2} = {output}");
         }
     }
 }
