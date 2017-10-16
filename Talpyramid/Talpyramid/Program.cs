ff4cf84543edef4d3c4c598680acd4197626a47f
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-10-16 08:29:05 +0200

    talpyramid init

---
+++ Talpyramid/Talpyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talpyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv ett tal: ");
+
+            int input;
+            bool validInput = int.TryParse(Console.ReadLine(), out input);
+            if (!validInput)
+            {
+                Console.WriteLine("Ogiltigt Input");
+                Main(args);
+            }
+
+            Console.WriteLine("Hej");
+
+            Console.ReadLine();
+        }
+    }
+}
