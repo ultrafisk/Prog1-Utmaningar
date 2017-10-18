9ba0fe78ef10c43fa9830bfa9136b4cd4c484684
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-10-18 09:17:06 +0200

    Talpyramid
    Talpyramid with comments

--- Talpyramid/Talpyramid/Program.cs
+++ Talpyramid/Talpyramid/Program.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Talpyramid
 {
-    class Program
+    class Program           //Erik Hacker
     {
         static void Main(string[] args)
         {
             Console.Write("Skriv ett tal: ");
 
+            // ---- PARSE ----
+            // omvandlar readline till integern "input"
             int input;
             bool validInput = int.TryParse(Console.ReadLine(), out input);
             if (!validInput)
@@ -20,7 +22,17 @@ namespace Talpyramid
                 Main(args);
             }
 
-            Console.WriteLine("Hej");
+            // ---- PYRAMID ----
+            //för varje i så skiver den inre loopen ut "input" i antal gånger
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($"{input}");
+                }
+
+                Console.Write("\n");
+            }
 
             Console.ReadLine();
         }
